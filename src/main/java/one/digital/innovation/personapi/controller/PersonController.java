cc78fb87f9e1b5f7f782fe4ff074628f514a722f
Pedro Gomes <pedro@ipb.org.br>
2021-02-24 17:53:19 -0300

    Improved person creaton functionality

--- src/main/java/one/digital/innovation/personapi/controller/PersonController.java
+++ src/main/java/one/digital/innovation/personapi/controller/PersonController.java
@@ -1,12 +1,14 @@
 package one.digital.innovation.personapi.controller;
 
-import one.digital.innovation.personapi.dto.MessageResponseDTO;
+import one.digital.innovation.personapi.dto.request.PersonDTO;
+import one.digital.innovation.personapi.dto.response.MessageResponseDTO;
 import one.digital.innovation.personapi.entity.Person;
-import one.digital.innovation.personapi.repository.PersonRepository;
 import one.digital.innovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -21,7 +23,7 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO createPerson(@RequestBody Person person){
-        return personService.createPerson(person);
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
+        return personService.createPerson(personDTO);
     }
 }
